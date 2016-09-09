969b87121e4c708952420de650be51a207278282
danylomaster@ukr.net <Perlova1715>
2016-09-09 13:54:47 +0300

    module1 solution added

--- solutions/module1/js/app.js
+++ solutions/module1/js/app.js
@@ -1,15 +1,30 @@
 (function() {
     'use strict';
-    angular.module('DIApp', [])
-        .controller('DIController', DIController);
+    angular.module('LCApp', [])
+    .controller('LCController', LCController);
 
-    DIController.$inject = ['$scope','$filter']; //dependency injection perfect for minification
-    function DIController($scope, $filter) {
-        $scope.name = "Danylo";
-        $scope.upper = function () {
-          var upCase = $filter('uppercase');
-          $scope.name = upCase($scope.name);
-        }
+    LCController.$inject = ['$scope']; //dependency injection perfect for minification
+    function LCController($scope) {
+        $scope.message = "";
+        $scope.foodList = "";
+        $scope.messageStyle;
+        $scope.checkIfTooMuch = function () {
+            var list = $scope.foodList.trim();
+            var style = {};
+            style.color = colorMessageText(list);
+            style['border'] = '1px solid '+borderColorMessageText(list);
+          $scope.message = formGroupMessage(list);
+          $scope.messageStyle = style;
+        };
     };
+    function formGroupMessage(list) {
+        return (list.length==0)?"Please enter data first":(list.split(",").length<=3)?"Enjoy!":"Too much!";
+    };
+    function colorMessageText(list) {
+        return (list.length==0)?'red':'green';
+    }
+    function borderColorMessageText(list) {
+        return (list.length==0)?'red':'green';
+    }
 
 })();
